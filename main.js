5296c83d3753531e65b167d6c02ee9332efa4d19
Milan <milanvpage@gmail.com>
2021-09-19 20:13:47 -0700

    commit tree 98b9978042413a32311ba0189be6d5ac15712710

--- main.js
+++ main.js
@@ -3,7 +3,42 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.getElementById('modal');
+modal.className = 'hidden';
+const modalMsg = document.getElementById('modal-message')
 
+const hearts = document.querySelectorAll(".like-glyph");
+
+for (let heart of hearts) {
+  heart.addEventListener("click", clickHeart);
+}
+
+function clickHeart(e) {
+  // debugger
+  console.log(`${e.target.innerHTML} clicked`);
+  let heart = e.target
+  mimicServerCall()
+  .then(function(serverMessage){
+    // debugger
+    alert(serverMessage);
+    if(heart.innerText == EMPTY_HEART) {
+      heart.innerText = FULL_HEART;
+      heart.classList.add('.activated-heart');
+    } else {
+      heart.innerText = EMPTY_HEART;
+      heart.classList.remove('.activated-heart');
+    }
+// debugger
+  })
+  .catch((error) => {
+
+    modal.classList.remove('hidden')
+    modalMsg.innerHTML += error
+    setTimeout(() => modal.className = 'hidden', 5000);
+
+  })
+
+}
 
 
 
